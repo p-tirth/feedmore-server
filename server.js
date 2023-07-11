de6f97989ac9b9f1fe0f4cb2f9ab0b851461aaa6
Tirth <tirthpatel2992003@gamail.com>
2023-07-12 04:48:29 +0530

    server

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const io = require("socket.io")(3000, {
     cors: {
       origin: [
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:5175",
-        "https://feedmore-ws-admin.vercel.app/",
-        "http://feedmore-ws-admin.vercel.app/",
-        "ws://feedmore-ws-admin.vercel.app/",
-        "wss://feedmore-ws-admin.vercel.app/",
-        "ws://feedmore-ws-client.vercel.app/",
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:5175',
+        'https://feedmore-ws-admin.vercel.app/',
+        'http://feedmore-ws-admin.vercel.app/',
+        'ws://feedmore-ws-admin.vercel.app/',
+        'wss://feedmore-ws-admin.vercel.app/',
+        'ws://feedmore-ws-client.vercel.app/',
       ],
       methods: ["GET", "POST"],
       allowedHeaders: ["my-custom-header"],
