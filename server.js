3cf5594eb4268f04fb1329ddbf9c88385bace564
Tirth <tirthpatel2992003@gamail.com>
2023-07-12 04:00:37 +0530

    admin

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const io = require("socket.io")(3000,{
         origin:['http://localhost:5173',
         'http://localhost:5174',
         'http://localhost:5175',
+        'https://feedmore-ws-admin.vercel.app/',
+        'ws://feedmore-ws-admin.vercel.app/',
         'wss://feedmore-ws-admin.vercel.app/',
         'ws://feedmore-ws-client.vercel.app/'],
     },
