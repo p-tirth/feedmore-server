6cdd07e59d5f89c0f98bb4fa2ebc81d1ee473fb4
Tirth <tirthpatel2992003@gamail.com>
2023-07-12 04:38:02 +0530

    server

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
-const io = require("socket.io")(3000,{
-    cors:{
-        origin:['http://localhost:5173',
-        'http://localhost:5174',
-        'http://localhost:5175',
-        'https://feedmore-ws-admin.vercel.app/',
-        'http://feedmore-ws-admin.vercel.app/',
-        'ws://feedmore-ws-admin.vercel.app/',
-        'wss://feedmore-ws-admin.vercel.app/',
-        'ws://feedmore-ws-client.vercel.app/',
-        'Access-Control-Allow-Origin: https://feedmore-ws-admin.vercel.app/',
-        'Access-Control-Allow-Origin: https://feedmore-ws-client.vercel.app/'],
+const io = require("socket.io")(3000, {
+    cors: {
+      origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:5175",
+        "https://feedmore-ws-admin.vercel.app/",
+        "http://feedmore-ws-admin.vercel.app/",
+        "ws://feedmore-ws-admin.vercel.app/",
+        "wss://feedmore-ws-admin.vercel.app/",
+        "ws://feedmore-ws-client.vercel.app/",
+      ],
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true,
     },
-})
-
-io.on("connection", (socket) => {
-  socket.on("adminConnection", (msg) => {
-    adminId = socket.id;
-    console.log(`Admin has connected with id :${adminId} and says ${msg}`);
   });
-  socket.on("clientConnection", (msg) => {
-    console.log(`client has connected with id :${socket.id} and says ${msg}`);
+  
+  let adminId;
+  
+  io.on("connection", (socket) => {
+    socket.on("adminConnection", (msg) => {
+      adminId = socket.id;
+      console.log(`Admin has connected with id: ${adminId} and says ${msg}`);
+    });
+    socket.on("clientConnection", (msg) => {
+      console.log(`Client has connected with id: ${socket.id} and says ${msg}`);
+    });
+    socket.on("foodInfo", (data) => {
+      console.log(`${data.name} sent to server by ${socket.id}`);
+      socket.broadcast.to(adminId).emit("foodInfo", data);
+      console.log("Data sent to admin");
+    });
   });
-  socket.on("foodInfo", (data) => {
-    console.log(`${{ ...data.name }} sent to server by ${socket.id}`);
-    socket.broadcast.to(adminId).emit("foodInfo", data);
-    console.log("data sent to admin");
-  });
-});
+  
